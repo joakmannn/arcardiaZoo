c110e98b087bfaa898fe659b19ffe7d9f3e34e58
joakmannn <joachim.asfalt@gmail.com>
2024-10-05 22:11:39 +0200

    Create VeterinaryReport.php

---
+++ app/Models/VeterinaryReport.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VeterinaryReport extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['date', 'details', 'animal_id'];
+
+    // Un rapport vétérinaire est lié à un animal
+    public function animal()
+    {
+        return $this->belongsTo(Animal::class);
+    }
+
+    // Un rapport vétérinaire est rédigé par un utilisateur
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
