4efe040075cf803b605f4421901874272c2166f6
joakmannn <joachim.asfalt@gmail.com>
2024-10-05 02:26:00 +0200

    Create 2024_10_05_002534_create_user_roles_table.php

---
+++ database/migrations/2024_10_05_002534_create_user_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id'); // Clé étrangère vers users
+            $table->unsignedBigInteger('role_id'); // Clé étrangère vers roles
+            $table->primary(['user_id', 'role_id']); // Clé primaire composée
+
+            // Définir les relations
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
