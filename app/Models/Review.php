96d7413898a815864010ee88d0c8aef8a4932340
joakmannn <joachim.asfalt@gmail.com>
2024-10-05 22:09:07 +0200

    Create Review.php

---
+++ app/Models/Review.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['username', 'comment', 'is_visible'];
+}
