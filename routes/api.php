5e741da92119ec1d0f65e6477fd4f75f4c383cc1
joakmannn <joachim.asfalt@gmail.com>
2024-10-06 12:53:50 +0200

    Implémentation routes api

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\BreedController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\HabitatController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\VeterinaryReportController;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+// Routes API pour les rôles
+Route::apiResource('roles', RoleController::class);
+
+// Routes API pour les services
+Route::apiResource('services', ServiceController::class);
+
+// Routes API pour les races (breeds)
+Route::apiResource('breeds', BreedController::class);
+
+// Routes API pour les images
+Route::apiResource('images', ImageController::class);
+
+// Routes API pour les animaux
+Route::apiResource('animals', AnimalController::class);
+
+// Routes API pour les habitats
+Route::apiResource('habitats', HabitatController::class);
+
+// Routes API pour les avis (reviews)
+Route::apiResource('reviews', ReviewController::class);
+
+// Routes API pour les rapports vétérinaires
+Route::apiResource('veterinary-reports', VeterinaryReportController::class);
+
