475d3d07c609225e1059431e1edbf134c0ba5519
joakmannn <joachim.asfalt@gmail.com>
2024-11-06 09:51:38 +0100

    Update VeterinaryReport.php

--- app/Models/VeterinaryReport.php
+++ app/Models/VeterinaryReport.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,17 +8,26 @@ class VeterinaryReport extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['date', 'details', 'animal_id', 'user_id'];
+    // Ajoutez 'feed_type' et 'feed_quantity' dans les champs fillables
+    protected $fillable = [
+        'date', 
+        'details', 
+        'animal_id', 
+        'user_id', 
+        'habitat_comment', 
+        'feed_type', 
+        'feed_quantity'
+    ];
 
-    // Un rapport vétérinaire est lié à un animal
+    // Un rapport vétérinaire est lié à un seul animal
     public function animal()
     {
-        return $this->belongsTo(Animal::class);
+        return $this->belongsTo(Animal::class, 'animal_id');
     }
 
-    // Un rapport vétérinaire est rédigé par un utilisateur
+    // Un rapport vétérinaire est rédigé par un utilisateur (vétérinaire)
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
