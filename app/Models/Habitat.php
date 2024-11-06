861e7fa8b15c9136e32ef3ef819817034d1f7f28
joakmannn <joachim.asfalt@gmail.com>
2024-11-06 09:50:58 +0100

    Update Habitat.php

--- app/Models/Habitat.php
+++ app/Models/Habitat.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Models;namespace App\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ConsultationHabitat; // Assurez-vous d'importer le modèle MongoDB
 
 class Habitat extends Model
 {
@@ -14,12 +15,18 @@ class Habitat extends Model
     // Un habitat peut avoir plusieurs animaux
     public function animals()
     {
-        return $this->belongsToMany(Animal::class, 'animal_habitat', 'habitat_id', 'animal_id');
+        return $this->belongsToMany(Animal::class, 'animal_habitats', 'habitat_id', 'animal_id');
     }
 
     // Un habitat peut avoir plusieurs images
     public function images()
     {
         return $this->belongsToMany(Image::class, 'image_habitat', 'habitat_id', 'image_id');
+    }
+
+    // Méthode pour récupérer les consultations depuis MongoDB
+    public function consultations()
+    {
+        return ConsultationHabitat::where('habitat_id', $this->id)->get();
     }
 }
