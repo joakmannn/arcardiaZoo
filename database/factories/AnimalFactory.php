b413addfa2c6ef817081a53a71a3c77bbedf61e7
joakmannn <joachim.asfalt@gmail.com>
2024-11-07 17:57:01 +0100

    Update AnimalFactory.php

--- database/factories/AnimalFactory.php
+++ database/factories/AnimalFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Breed;
-use App\Models\Image;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Animal>
