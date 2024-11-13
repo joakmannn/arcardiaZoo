77c124f6d39454ff21452c185e2b54e240359492
joakmannn <joachim.asfalt@gmail.com>
2024-11-13 10:57:45 +0100

    Update Home.jsx

--- resources/js/Pages/Client/Home.jsx
+++ resources/js/Pages/Client/Home.jsx
@@ -1,3 +1,4 @@
+// Home.jsx
 import React from 'react';
 import { usePage } from '@inertiajs/react';
 import Navigation from './components/Navigation';  
@@ -7,17 +8,18 @@ import ReviewsClient from './ReviewsClient';
 import ContactsClient from './ContactsClient';
 import Accueil from './components/Accueil';
 import CookieBanner from './CookieBanner';
+import Footer from './Footer';
 
 const Home = () => {
     // Récupérer les services et habitats via Inertia
-    const { services, habitats, animals} = usePage().props;
+    const { services, habitats, animals } = usePage().props;
     console.log(services, habitats);  // Vérifier si les données sont correctement reçues
 
     return (
         <div className="min-h-screen flex flex-col">
             {/* Navigation en haut */}
             <Navigation />
-            <main className="pt-16 relative"> 
+            <main className="pt-16 flex-grow"> 
                 <Accueil services={services} habitats={habitats} animals={animals}/>
                 <ServicesClient services={services} /> 
                 <HabitatsClient habitats={habitats} /> 
@@ -25,6 +27,8 @@ const Home = () => {
                 <ContactsClient />
                 <CookieBanner/>
             </main>
+                <Footer />
+
         </div>
     );
 };
