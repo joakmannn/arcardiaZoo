def5656aef6a15d5507a480c289fb58a2f1b088e
joakmannn <joachim.asfalt@gmail.com>
2024-11-15 12:59:54 +0100

    Update Home.jsx

--- resources/js/Pages/Client/Home.jsx
+++ resources/js/Pages/Client/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { usePage } from '@inertiajs/react';
-import Navigation from './components/Navigation';  
+import Navigation from './components/Navigation';
 import ServicesClient from './ServicesClient';
 import HabitatsClient from './HabitatsClient';
 import ReviewsClient from './ReviewsClient';
@@ -8,40 +8,54 @@ import ContactsClient from './ContactsClient';
 import Accueil from './components/Accueil';
 import CookieBanner from './CookieBanner';
 import Footer from './Footer';
+import useInViewAnimation from './useInViewAnimation';
 
 const Home = () => {
     const { services, habitats, animals, approvedReviews } = usePage().props;
     const [activeSection, setActiveSection] = useState(null);
+
+    const [servicesRef, isServicesInView] = useInViewAnimation(0.5); 
+    const [habitatsRef, isHabitatsInView] = useInViewAnimation(0.5);
+    const [reviewsRef, isReviewsInView] = useInViewAnimation(0.5);
+    const [contactsRef, isContactsInView] = useInViewAnimation(0.5);
 
     const handleMouseEnter = (section) => {
         setActiveSection(section);
     };
 
     const handleMouseLeave = () => {
-        setActiveSection(null); // Reset to initial state on mouse leave
+        setActiveSection(null);
     };
 
     return (
         <div className="min-h-screen flex flex-col">
             <Navigation />
-            <main className="pt-16 flex-grow"> 
+            <main className="pt-8 pb-8 flex-grow">
                 <Accueil services={services} habitats={habitats} animals={animals} approvedReviews={approvedReviews} />
 
-                {/* Services and Habitats sections */}
-                <div className="flex flex-col md:flex-row w-full min-h-screen">
+                <div className="flex flex-col md:flex-row w-full min-h-[70vh]">
                     <div
-                        className={`transition-all duration-500 ease-in-out ${
-                            activeSection === 'services' ? 'w-full' : 'w-full md:w-1/2'
-                        } flex justify-center`}
+                        ref={servicesRef}
+                        className={`flex justify-center items-center transition-all ${
+                            isServicesInView ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'
+                        } ${activeSection === 'services' ? 'w-full' : 'w-full md:w-1/2'} py-8 px-4`}
+                        style={{
+                            transitionDuration: isServicesInView ? (window.innerWidth < 768 ? '1500ms' : '700ms') : '700ms',
+                        }}
                         onMouseEnter={() => handleMouseEnter('services')}
                         onMouseLeave={handleMouseLeave}
                     >
                         <ServicesClient />
                     </div>
+                    
                     <div
-                        className={`transition-all duration-500 ease-in-out ${
-                            activeSection === 'habitats' ? 'w-full' : 'w-full md:w-1/2'
-                        } flex justify-center`}
+                        ref={habitatsRef}
+                        className={`flex justify-center items-center transition-all ${
+                            isHabitatsInView ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'
+                        } ${activeSection === 'habitats' ? 'w-full' : 'w-full md:w-1/2'} py-8 px-4`}
+                        style={{
+                            transitionDuration: isHabitatsInView ? (window.innerWidth < 768 ? '1500ms' : '700ms') : '700ms',
+                        }}
                         onMouseEnter={() => handleMouseEnter('habitats')}
                         onMouseLeave={handleMouseLeave}
                     >
@@ -49,21 +63,28 @@ const Home = () => {
                     </div>
                 </div>
 
-                {/* Reviews and Contacts sections */}
                 <div className="flex flex-col md:flex-row w-full">
-                    <div 
-                        className={`flex-1 flex justify-center transition-all duration-500 ease-in-out ${
-                            activeSection === 'reviews' ? 'w-full' : 'w-full md:w-1/2'
-                        }`}
+                    <div
+                        ref={reviewsRef}
+                        className={`flex-1 flex justify-center items-center transition-all ${
+                            isReviewsInView ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+                        } py-8 px-4`}
+                        style={{
+                            transitionDuration: isReviewsInView ? (window.innerWidth < 768 ? '1500ms' : '700ms') : '700ms',
+                        }}
                         onMouseEnter={() => handleMouseEnter('reviews')}
                         onMouseLeave={handleMouseLeave}
                     >
                         <ReviewsClient isHovered={activeSection === 'reviews'} />
                     </div>
-                    <div 
-                        className={`flex-1 flex justify-center transition-all duration-500 ease-in-out ${
-                            activeSection === 'contacts' ? 'w-full' : 'w-full md:w-1/2'
-                        }`}
+                    <div
+                        ref={contactsRef}
+                        className={`flex-1 flex justify-center items-center transition-all ${
+                            isContactsInView ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+                        } py-8 px-4`}
+                        style={{
+                            transitionDuration: isContactsInView ? (window.innerWidth < 768 ? '1500ms' : '700ms') : '700ms',
+                        }}
                         onMouseEnter={() => handleMouseEnter('contacts')}
                         onMouseLeave={handleMouseLeave}
                     >
