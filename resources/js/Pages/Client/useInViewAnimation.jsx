5071aa544cee317f3685c43bafddaa10dcc04f46
joakmannn <joachim.asfalt@gmail.com>
2024-11-15 13:04:55 +0100

    Create useInViewAnimation.jsx

---
+++ resources/js/Pages/Client/useInViewAnimation.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useInViewAnimation = (threshold = 0.5) => { // Set threshold to 0.5
+    const ref = useRef(null);
+    const [isInView, setIsInView] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsInView(entry.isIntersecting);
+            },
+            { threshold }
+        );
+
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, [threshold]);
+
+    return [ref, isInView];
+};
+
+export default useInViewAnimation;
