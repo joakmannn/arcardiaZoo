f05029ccd185f64ba430975a53e1f44f7cbc08f7
joakmannn <joachim.asfalt@gmail.com>
2024-11-19 09:31:40 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HabitatController;
 use App\Http\Controllers\VeterinaryReportController;
